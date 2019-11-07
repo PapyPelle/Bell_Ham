ff66c80f876053d08419e34a340670d6992e2f32
KalzCoat <kalzcoat@gmail.com>
2019-11-07 17:54:14 -0500

    Ajout de la scène de menu principal et de son script

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+
+	public void Continue()
+	{
+		Debug.Log(" -- Continuer --");
+	}
+
+	public void NewGame()
+	{
+		Debug.Log(" -- Nouvelle partie --");
+	}
+
+	public void Options()
+	{
+		Debug.Log(" -- Options --");
+	}
+
+    public void Quit()
+    {
+    	Debug.Log(" -- Quitter --");
+        Application.Quit();
+    }
+}
