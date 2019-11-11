2432ac223770170835a03db2e86a2d729c41fb00
KalzCoat <kalzcoat@gmail.com>
2019-11-11 17:24:14 -0500

    Création de perso (suite)

---
+++ Assets/Scripts/UI/BookPopup.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookPopup : MonoBehaviour
+{
+
+	public GameObject popup;
+
+	int num_capacity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        popup.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void capacity1() {
+    	popup.SetActive(true);
+    	num_capacity = 1;
+    }
+    public void capacity2() {
+    	popup.SetActive(true);
+    	num_capacity = 2;
+    }
+    public void capacity3() {
+    	popup.SetActive(true);
+    	num_capacity = 3;
+    }
+    public void capacity4() {
+    	popup.SetActive(true);
+    	num_capacity = 4;
+    }
+
+
+    public void SetCapacity() {
+    	popup.SetActive(false);
+		if (num_capacity==1) {
+
+		}
+		if (num_capacity==2) {
+			
+		}
+		if (num_capacity==3) {
+			
+		}
+		if (num_capacity==4) {
+			
+		}
+    }
+
+
+
+}
---
+++ Assets/Scripts/UI/CapacityBook.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapacityBook : MonoBehaviour
+{
+
+	public GameObject P1;
+	public GameObject P2;
+	public GameObject P3;
+	public GameObject P4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        P1.SetActive(true);
+        P2.SetActive(false);
+        P3.SetActive(false);
+        P4.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void b1() {
+        P1.SetActive(true);
+        P2.SetActive(false);
+        P3.SetActive(false);
+        P4.SetActive(false);
+    }
+
+    public void b2() {
+        P1.SetActive(false);
+        P2.SetActive(true);
+        P3.SetActive(false);
+        P4.SetActive(false);
+    }
+
+    public void b3() {
+        P1.SetActive(false);
+        P2.SetActive(false);
+        P3.SetActive(true);
+        P4.SetActive(false);
+    }
+
+    public void b4() {
+        P1.SetActive(false);
+        P2.SetActive(false);
+        P3.SetActive(false);
+        P4.SetActive(true);
+    }
+}
