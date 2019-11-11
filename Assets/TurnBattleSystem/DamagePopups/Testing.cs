62f9ccffcc518eccfbbf483bccb5c8b738c0f63e
Fussien <Fussienpaul@hotmail.fr>
2019-11-10 22:27:42 -0500

    Ajout partie jeu
    Fichier animation, sprite, jeu

---
+++ Assets/TurnBattleSystem/DamagePopups/Testing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour {
+
+    private void Start() {
+        //DamagePopup.Create(Vector3.zero, 300);
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            bool isCriticalHit = Random.Range(0, 100) < 30;
+            //DamagePopup.Create(UtilsClass.GetMouseWorldPosition(), 100, isCriticalHit);
+        }
+    }
+}
