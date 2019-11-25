a6184562926be2fc49b6a944bfebc8a5fc402047
Jonathan <casabona.jonathan@gmail.com>
2019-11-24 23:48:51 -0500

    Ajout Event scene
    ajout d'une scène pour afficher les evenement autre que les combats, exemple marchand

---
+++ Assets/Scripts/EventScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EventScript : MonoBehaviour
+{
+   
+    public AudioClip confirm;
+    public AudioClip cancel;
+    /* public AudioClip music;
+
+    public GameObject canvas; // this*/
+    public GameObject BoxDialog,TextDialog;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string text="";
+
+        switch(Random.Range(0, 2))
+        {
+            case 0:
+                text = "Encore un pigeon a plumer ! ";
+                break;
+            case 1:
+                text = "Bonjour étranger, serais tu interessés par quelques potions ? ! ";
+                break;
+
+            case 2:
+                text = "Interessé par quelques choses ? ";
+                break;
+
+
+        }
+        TextDialog.transform.GetComponent<Text>().text = text;//Changing text
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void btnYes()
+    {
+        SoundManager.instance.RandomizeSfx(confirm);
+        BoxDialog.SetActive(false);
+
+    }
+    public void btnNo()
+    {
+        SoundManager.instance.RandomizeSfx(cancel);
+        SceneManager.LoadScene("Map");
+    }
+}
