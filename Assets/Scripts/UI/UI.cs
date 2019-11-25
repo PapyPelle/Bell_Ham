b2948a79d2c4d552e748080512966956596f682b
KalzCoat <kalzcoat@gmail.com>
2019-11-25 16:45:03 -0500

    Inventaire V.1

---
+++ Assets/Scripts/UI/UI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+
+
+	public GameObject InventoryPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InventoryPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickInventory() {
+    	InventoryPanel.SetActive(true);
+    	Debug.Log("test");
+    }
+    public void OnQuitInventory() {
+    	InventoryPanel.SetActive(false);
+    }
+}
