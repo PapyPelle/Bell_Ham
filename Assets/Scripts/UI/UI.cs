39015e9fe7549bfdacf0484b7f3a8008ff1b5fbe
KalzCoat <kalzcoat@gmail.com>
2019-12-02 12:50:18 -0500

    Equipement bidon

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -8,10 +8,18 @@ public class UI : MonoBehaviour
 
 
 	public GameObject InventoryPanel;
+	public GameObject DescriptionEq1;
+	public GameObject DescriptionEq2;
+
+	int description1AFF = 0;
+	int description2AFF = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         InventoryPanel.SetActive(false);
+        DescriptionEq1.SetActive(false);
+        DescriptionEq2.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,5 +34,31 @@ public class UI : MonoBehaviour
     }
     public void OnQuitInventory() {
     	InventoryPanel.SetActive(false);
+    	DescriptionEq1.SetActive(false);
+        DescriptionEq2.SetActive(false);
+    }
+
+    public void clickDescription1() {
+    	if (description1AFF==1) {
+			DescriptionEq1.SetActive(false);
+			description1AFF = 0;
+		} else {
+			DescriptionEq2.SetActive(false);
+			description2AFF = 0;
+			DescriptionEq1.SetActive(true);
+			description1AFF = 1;
+		}
+    }
+
+    public void clickDescription2() {
+    	if (description2AFF==1) {
+			DescriptionEq2.SetActive(false);
+			description2AFF = 0;
+		} else {
+			DescriptionEq1.SetActive(false);
+			description1AFF = 0;
+			DescriptionEq2.SetActive(true);
+			description2AFF = 1;
+		}
     }
 }
